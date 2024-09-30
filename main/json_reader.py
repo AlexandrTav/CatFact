2c703234c35a81a49e1c4b5fd3358a1d6b6cf285
JustEffect23 <mr.effect23gmail.com>
2024-09-30 19:00:47 +0200

    errors30.9.

--- main/json_reader.py
+++ main/json_reader.py
@@ -8,5 +8,11 @@ class JsonReader:
         self.base_url = base_url
         self.api_key = api_key
 
+    def TempData(self):
+        response = requests.get(self.base_url)
+        data = response.json()
+        return data["current"]["temp_c"]
+
     def get_response_json(self):
-        return requests.get(self.base_url).json()
+        response = requests.get(self.base_url)
+        return response.json()
--- main/main.py
+++ main/main.py
@@ -5,23 +5,23 @@ from history import History
 
 
 app = Flask(__name__)
-reader = JsonReader("https://catfact.ninja/fact", "")
+reader = JsonReader
+#f"http://api.weatherapi.com/v1/current.json?key&q=Brno"
+api_key = "68870090a3624a80ba2104715242309"
+base_url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q=Brno"
+
+
+reader = JsonReader(base_url, api_key)
+
 history = History()
 app.secret_key = "VerySecretKey123@!#"
 
 response_data = reader.get_response_json()
 
-""" 
-Create main page from template
-User can login to add his name to history, and can work as a Guest.
-Username is saved to the session.
-Accepts POST and GET requests. 
-POST request is for accepting username.
-GET request for all other requests
-Cat image width depends on fact length
-"""
+#temperature = data.current.temp_c
+
 @app.route("/", methods = ['POST', 'GET'])
-def main_page(cat_data = response_data):
+def main_page(weather_data = response_data):
     # Create main page from template. Pass data to this page.
     if 'username' in session:
         username = session['username']
@@ -29,13 +29,28 @@ def main_page(cat_data = response_data):
         username = 'Guest'
 
     if request.method == 'GET':
-        cat_data = reader.get_response_json()
-        history.addRecord(username + ":" + str(cat_data))
+        weather_data = reader.get_response_json()
+        history.addRecord(username + ":" + str(weather_data))
     if request.method == 'POST':
         session['username'] = request.form['txt_username']
-        return render_template("main.html", data=cat_data, user=session['username'])
+        return render_template("main.html", data=weather_data, user=session['username'])
 
-    return render_template('main.html', data=cat_data, user=username)
+    return render_template('main.html', data=weather_data, user=username)
+
+"""
+def WhatToWear:
+  if temperature < 0:
+        return "coat.png", "hat.png"
+  elif temperature => 0 && temperature < 10:
+      return "jacket.png", "cap.png"
+  elif temperature => 10 && temperature <= 20:
+      return "jacket.png", "cap.png"
+  elif temperature > 20:
+      return "jacket.png", "cap.png" 
+"""
+
+
+
 
 """
 Page shows history of main page usage. Reads this data from the file.
