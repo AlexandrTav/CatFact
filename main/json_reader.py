9e6b65f5d7ed6c7348ad00ae4af6f9aefd80dcb4
JustEffect23 <mr.effect23gmail.com>
2024-10-02 21:38:58 +0200

    errors30.9.

--- main/json_reader.py
+++ main/json_reader.py
@@ -1,23 +1,38 @@
 import requests
 
+from datetime import datetime
+
 """
 Reads data from free Cat facts API
 """
 class JsonReader:
     def __init__(self, base_url, api_key):
-        self.base_url = base_url
+        self.url = base_url + f"?key={api_key}"
         self.api_key = api_key
 
     def cityWeather(self, city="Moscow"):
-        self.base_url = f"http://api.weatherapi.com/v1/current.json?key={self.api_key}&q={city}"
-        response = requests.get(self.base_url)
+        self.url = self.url + f"&q={city}"
+        response = requests.get(self.url)
         return response.json()
 
-    def TempData(self):
-        response = requests.get(self.base_url)
-        data = response.json()
-        return data["current"]["temp_c"]
+    def H_Time(self):
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    def TempData(self, city="Moscow"):
+        response = requests.get(self.url)
+        data = self.cityWeather(city)
+        temp = data["current"]["temp_c"]
+        cond = data["current"]["condition"]["text"]
+        return temp, cond
+
+    def HistoryInfo(now, city, data):
+        {
+            "time": now,
+            "city": city,
+            "temperature": data,
+            #"condition": cond
+        }
 
     def get_response_json(self):
-        response = requests.get(self.base_url)
-        return response.json()
+        return self.cityWeather()
